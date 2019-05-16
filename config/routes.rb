5aa925bd036a958d18452d6583dcdc08ecda27b2
Robert Simon <robertsimonmedia@gmail.com>
2019-05-16 15:09:43 -0400

    added controllers

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :cocktails do
     resources :ingredients, only: [:create, :edit]
-    resources :doses, only: [:create, :edit]
+    resources :doses, only: [:create, :new, :delete]
   end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,15 +1,18 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+require 'open-uri'
+require 'json'
 
-Dose.destroy_all
-Ingredient.destroy_all
-Cocktail.destroy_all
+url = 'https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list'
+user_serialized = open(url).read
+api_return = JSON.parse(user_serialized)
 
-Cocktail.create!(name: 'The other flaming bird')
-Ingredient.create!(name: 'Pineapple')
-Dose.create!(description: 'put pineapple in the bird', cocktail: Cocktail.first, ingredient: Ingredient.first)
+drinks = []
+api_return['drinks'].map do |drink|
+  drink_ingredients = drink.values
+  drink_ingredients.each do |ingredient|
+    drinks << ingredient
+  end
+end
+
+5.times do
+  ingredient.create(name: drinks.rand)
+end
